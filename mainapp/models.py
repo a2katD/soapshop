75f6ba1acc99b60e4dcb3df86b50b91de5a769a4
a2kat <mousedrw@ya.ru>
2022-07-09 15:17:47 +0300

    edit urls, add media folder

--- mainapp/models.py
+++ mainapp/models.py
@@ -25,6 +25,8 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.name}'
 
 class Products(models.Model):
     name = models.CharField(max_length=128)
@@ -33,3 +35,6 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,17 +14,11 @@ https://docs.djangoproject.com/en/4.0/topics/http/urls/
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from mainapp.views import products
 
-from mainapp.views import index, products
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='mainapp'))
+    path('', products, name='products'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
