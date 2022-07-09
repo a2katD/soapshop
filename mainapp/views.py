b4cfedf358c063f03e403c697a25f4b47cc8ef65
a2kat <mousedrw@ya.ru>
2022-07-09 13:38:32 +0300

    add pleaseholder in product.html, add json

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,12 @@
 # Представления(контроллеры), они импортируются в urls, и описывают действия, при их вызове чере urls
 # В нашем случае они рендерят(отображают) страницу
 
+import json
+import os
+
 from django.shortcuts import render
+
+MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
@@ -12,7 +17,16 @@ def index(request):
 
 
 def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context = {
         'title': 'Soapshop - Каталог',
     }
+    context['products'] = json.load(open(file_path, encoding='utf-8'))
+    context['categories'] = [
+        'Новинки',
+        'Одежда',
+        'Обувь',
+        'Аксессуары',
+        'Подарки',
+    ]
     return render(request, 'mainapp/products.html', context)
