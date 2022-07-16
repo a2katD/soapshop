de8bb8dba82c86fc0235c245b3fa91566b76fb83
a2kat <mousedrw@ya.ru>
2022-07-16 16:41:24 +0300

    add profile

--- authapp/urls.py
+++ authapp/urls.py
@@ -16,8 +16,13 @@ https://docs.djangoproject.com/en/4.0/topics/http/urls/
 """
 from django.urls import path
 
-# from authapp.views import
+from authapp.views import login, register, profile, logout
+
+app_name = 'authapp'
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,75 @@
+from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        # else:
+        #     print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'Geekshop | Авторизация',
+        'form': form,
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return HttpResponseRedirect(reverse('authapp:login'))
+        # else:
+        #     print(form.errors)
+    else:
+        form = UserRegisterForm()
+    context = {
+        'title': 'Soapshop | Регистрация',
+        'form': form,
+    }
+    return render(request, 'authapp/register.html', context)
+
+
+def profile(request):
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+    context = {
+        'title': 'Soapshop | Профиль',
+        'form': UserProfileForm(instance=request.user),
+    }
+    return render(request, 'authapp/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'mainapp/index.html', )
+
+# def remember(request):s
+#     """Создать функцию востановления пароля"""
+#     pass
+#     context = {
+#         'title': 'Soapshop | Восстановление пароля',
+#         'form': "",
+#     }
+#     return render(request, 'authapp/remember.html', context)
